cadb3108340f351a3a24be12d6cd4927410490e2
Viet Yen Nguyen <vyn@hypefactors.com>
2021-11-30 09:47:31 +0100

    fix: gradle
    Signed-off-by: Viet Yen Nguyen <vyn@hypefactors.com>

---
+++ bindings/java/src/main/java/co/huggingface/tokenizers/App.java
@@ -0,0 +1,8 @@
+package co.huggingface.tokenizers;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ bindings/java/src/main/java/co/huggingface/tokenizers/JnaTokenizer.java
@@ -0,0 +1,1 @@
+package co.huggingface.tokenizers;
