3dbb78cc4357cb878fbbab44525e0a6269ecd7ac
Viet Yen Nguyen <vyn@hypefactors.com>
2021-12-01 08:52:25 +0100

    chore: my first junit test
    Signed-off-by: Viet Yen Nguyen <vyn@hypefactors.com>

---
+++ bindings/java/src/test/java/co/huggingface/tokenizers/MicroBenchmark.java
@@ -0,0 +1,13 @@
+package co.huggingface.tokenizers;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MicroBenchmark {
+
+    @Test
+    void testFoo() {
+        System.out.println("foo");
+        assertEquals(0,1);
+    }
+}
