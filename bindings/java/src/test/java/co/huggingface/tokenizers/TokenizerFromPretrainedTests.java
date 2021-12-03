0f97160b4db160310008f5dcd75f7e53ca960d53
andreaduque <ad@hypefactors.com>
2021-12-03 09:33:46 +0100

    test unigram tokenizer

--- bindings/java/src/test/java/co/huggingface/tokenizers/TokenizerFromPretrainedTests.java
+++ bindings/java/src/test/java/co/huggingface/tokenizers/TokenizerFromPretrainedTests.java
@@ -7,12 +7,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TokenizerFromPretrainedTests {
 
-
-    @Test
+   @Test
     void testEncodeWordpiece() {
-        var tokenizer = new TokenizerFromPretrained("bert-base-cased");
+        var tokenizer = TokenizerFromPretrained.create("bert-base-cased").value();
         var input = "Tokenize me please!";
-        var encodings = tokenizer.encode(input, true);
+        var encodings = tokenizer.encode(input, true).value();
 
         var expectedTokens = new String[] {"[CLS]", "To", "##ken", "##ize", "me", "please", "!", "[SEP]"};
         var expectedIds = new long[] {101L, 1706L, 6378L, 3708L, 1143L, 4268L, 106L, 102L};
@@ -34,8 +33,26 @@ public class TokenizerFromPretrainedTests {
     //cannot fetch model from "google/mt5-base"
     @Test
     void testEncodeUnigram() {
-//        System.out.println("foo");
-        assertEquals(0,0);
+        var tokenizer = TokenizerFromPretrained.create("t5-small").value();
+        var input = "Tokenize me please!";
+        var encodings = tokenizer.encode(input, true).value();
+
+        var expectedTokens = new String[] {"▁To", "ken", "ize", "▁me", "▁please", "!", "</s>"};
+        var expectedIds = new long[] {304L, 2217L, 1737L, 140L, 754L, 55L, 1L};
+        var expectedTypeIds = new long[] {0L, 0L, 0L, 0L, 0L, 0L, 0L};
+        var expectedWordIds = new long[] {0L, 0L, 0L, 1L, 2L, 2L, -1L};
+
+        var tokens = encodings.getTokens();
+        var ids = encodings.getIds();
+        var typeIds = encodings.getTypeIds();
+        var wordIds = encodings.getWordIds();
+
+
+        assertArrayEquals(expectedTokens,tokens);
+        assertArrayEquals(expectedIds, ids);
+        assertArrayEquals(expectedTypeIds, typeIds);
+        assertArrayEquals(expectedWordIds, wordIds);
+
     }
 
     void testEncodeBPE() {
